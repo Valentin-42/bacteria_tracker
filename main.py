deea8162c1a94da71c4b342f7c612428fb904e12
Valentin <valentinvial42.vv@gmail.com>
2023-02-08 14:55:14 +0100

    contrast

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import cv2
+import numpy
+
 class Model :
 
     def __init__(self, path_to_ds) :
@@ -19,4 +22,21 @@ python train.py --img 256 --batch 16 --epochs 3 --data IMGS10.yaml --weights yol
         print("\n")
         print(l)
 
-    
+
+f = "raw/images/img0.jpg"
+img = cv2.imread(f)
+img = cv2.resize(img, (round(img.shape[1] / 4), round(img.shape[0] / 4)))
+
+img_h, img_w = img.shape[:2]
+img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+img1 = img.copy()
+img2 = img.copy()
+img3 = img.copy()
+
+img1 = cv2.convertScaleAbs(img1, alpha=3.0, beta=-10)
+
+
+cv2.imshow("Image C", img1)
+cv2.imshow("Image O", img2)
+cv2.waitKey(0)
