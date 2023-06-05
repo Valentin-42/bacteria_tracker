262c72f8cbd2a62260632ff3a3900643106bdc99
Cédric Pradalier <cedric.pradalier@georgiatech-metz.fr>
2023-06-05 17:28:51 +0200

    Saving the bacteria training parameters

--- yolov5/mytrain.sh
+++ yolov5/mytrain.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Training with yolov5"
-python train.py --img 256 --batch 128 --epochs 524288 --data 1bacteria.yaml --weights runs/train/yolov5s_fulldataset5/weights/last.pt --name yolov5s_fulldataset --patience 0
+python train.py --img 256 --batch 128 --epochs 524288 --data 2bacteria.yaml --weights runs/train/yolov5s_split_annotated_20230501/weights/best.pt --name yolov5s_split_annotated_20230501_cluster --patience 0 --device 1
--- yolov5/ws/run_all.sh
+++ yolov5/ws/run_all.sh
@@ -4,9 +4,9 @@ set -e
 
 for d in /mnt/bacteria/dataset/*; do 
     bn=`basename "$d"`_cp
-    # rm -rf /mnt/gpu_storage/bacteria_tracker/yolov5/runs/detect/"$bn"
-    # ./run_detection.sh "${d}" "$bn" /mnt/gpu_storage/bacteria_tracker/yolov5/runs/detect
-    ./run_kalman.sh "${d}/raw" "$bn" /mnt/gpu_storage/bacteria_tracker/yolov5/runs/detect
-    ./run_finalize.sh "${d}/raw" "$bn" /mnt/gpu_storage/bacteria_tracker/yolov5/runs/detect
+    rm -rf /mnt/gpu_storage/bacteria_tracker/yolov5/runs/detect/"$bn"
+    ./run_detection.sh "${d}" "$bn" /mnt/gpu_storage/bacteria_tracker/yolov5/runs/detect
+    # ./run_kalman.sh "${d}/raw" "$bn" /mnt/gpu_storage/bacteria_tracker/yolov5/runs/detect
+    # ./run_finalize.sh "${d}/raw" "$bn" /mnt/gpu_storage/bacteria_tracker/yolov5/runs/detect
 done
 
